41627251104646fcd8bbb9064bcc96336db47b33
Bruno Martins <bgcngm@gmail.com>
2024-06-30 15:43:03 +0100

    sm7325-common: Switch to health AIDL HAL
    Change-Id: I0600fbe80dbd538dfe2aef0538b68e4e399c915e

--- common.mk
+++ common.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.cas@1.2-service
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images_vendor
+
 # Configstore
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
@@ -203,6 +207,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health-service.samsung \
+    android.hardware.health-service.samsung-recovery \
     android.hardware.health@2.1.vendor
 
 # HIDL
@@ -401,9 +407,6 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v33
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
 
 # Weaver
 PRODUCT_PACKAGES += \
