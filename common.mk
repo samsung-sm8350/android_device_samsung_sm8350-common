f35bd76ca21454fc19c7b36fffe4a31d53881988
opensourcefreak <john@snurr.nu>
2024-12-01 11:16:09 +0100

    Migrate to qti power hal

--- common.mk
+++ common.mk
@@ -338,11 +338,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(COMMON_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QCOM
 PRODUCT_PACKAGES += \
