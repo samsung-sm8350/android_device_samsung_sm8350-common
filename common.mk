b51c5179b91a627942ac6679235ec5e62ba5dbbb
opensourcefreak <john@snurr.nu>
2025-03-25 15:32:30 +0100

    sm8350: Remove unused namespace

--- common.mk
+++ common.mk
@@ -445,7 +445,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/samsung \
     hardware/qcom-caf/sm8350
 
